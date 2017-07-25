82af688b457d26f8f710062b1f33008305e59472
Terence Zhao <terencezhao@chicagobooth.edu>
2017-07-25 05:17:45 +0000

    fixed director id

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,7 +34,7 @@ class MoviesController < ApplicationController
     @movie.duration = params[:duration]
     @movie.description = params[:description]
     @movie.image_url = params[:image_url]
-    @movie.director = params[:director_id]
+    @movie.director_id = params[:director_id]
     @movie.save
 
     render("show")
