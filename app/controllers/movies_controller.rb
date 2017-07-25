a7adf34107a0ea96bbebcd53e8468d195bf91d4e
Terence Zhao <terencezhao@chicagobooth.edu>
2017-07-25 05:04:07 +0000

    again

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -34,6 +34,7 @@ class MoviesController < ApplicationController
     @movie.duration = params[:duration]
     @movie.description = params[:description]
     @movie.image_url = params[:image_url]
+    @movie.director = params[:director_id]
     @movie.save
 
     render("show")
