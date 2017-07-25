a6d96c82f294ada6ee85e437d3f3a41808d6bcbf
Terence Zhao <terencezhao@chicagobooth.edu>
2017-07-25 03:56:58 +0000

    initial commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,19 +4,19 @@ class MoviesController < ApplicationController
   end
 
   def show
-    @movie = Movie.find(params[id])
+    @movie = Movie.find(params[:id])
   end
 
   def new_form
   end
 
   def create_row
+    @movie = Movie.new
     @movie.title = params[:the_title]
     @movie.year = params[:the_year]
     @movie.duration = params[:the_duration]
     @movie.description = params[:the_description]
     @movie.image_url = params[:the_image_url]
-    @movie.director_id = params[:the_director_id]
 
     @movie.save
 
@@ -28,19 +28,20 @@ class MoviesController < ApplicationController
   end
 
   def update_row
+    @movie = Movie.find(params[:id])
     @movie.title = params[:title]
     @movie.year = params[:year]
     @movie.duration = params[:duration]
     @movie.description = params[:description]
     @movie.image_url = params[:image_url]
-    @movie.director_id = params[:director_id]
+    @movie.save
 
     render("show")
   end
 
   def destroy
-    movie = Movie.find(params[:id])
+    @movie = Movie.find(params[:id])
 
-    movie.destroy
+    @movie.destroy
   end
 end
