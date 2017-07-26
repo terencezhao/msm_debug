c499df2927a45783dc73a8cf76820dc3dafdca08
Terence Zhao <terencezhao@chicagobooth.edu>
2017-07-26 06:40:47 +0000

    add director id to create movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,6 +17,7 @@ class MoviesController < ApplicationController
     @movie.duration = params[:duration]
     @movie.description = params[:description]
     @movie.image_url = params[:image_url]
+    @movie.director_id = params[:director_id]
 
     @movie.save
 
